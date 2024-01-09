72c5f80aaa346b6069a1b375fc2404c9c909840e
Michael C. Beck <14676+mcb@users.noreply.github.com>
2024-01-09 13:30:17 +0100

    added snapserver script

---
+++ snapserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -f /var/run/dbus/pid
+rm -f /var/run/avahi-daemon/pid
+
+dbus-daemon --system 
+avahi-daemon -D
+
+if [ $# -gt 1 ]; then
+	snapserver "$@"
+else
+	echo "No arguments for snapserver given, please provide arguments."
+fi
